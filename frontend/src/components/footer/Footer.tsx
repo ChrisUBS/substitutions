a83dfe5ccd33eed1a1653a6a6b7997ebbb483abd
Eduardo Becerra <josebecerra.0870@gmail.com>
2025-11-11 16:49:57 -0800

    Design login page and footer

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -2,13 +2,10 @@ function Footer() {
     const year = new Date().getFullYear();
 
     return (
-        <footer className="bg-gray-700 text-gray-100 py-3 text-center text-sm">
+        <footer className="bg-[#333333] text-white font-medium py-3 text-center text-sm">
             <div className="container mx-auto px-4">
                 <p className="leading-tight">
-                    CIMATANK © {year}. Todos los derechos reservados.
-                </p>
-                <p className="text-gray-200 text-xs sm:text-sm">
-                    Sistema desarrollado por alumnos de Ingeniería en Software y Tecnologías Emergentes (FCITEC).
+                    San Diego Global Knowledge University © {year}. All rights reserved.
                 </p>
             </div>
         </footer>
